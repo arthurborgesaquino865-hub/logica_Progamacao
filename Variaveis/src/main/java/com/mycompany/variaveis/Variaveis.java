aa9611e24e0e94382c61279c204d235205ab631e
arthurborgesaquino865-hub <arthurborgesaquino865@gmail.com>
2026-09-02 17:53:24 -0300

    aprendendo sobre variaveis

---
+++ Variaveis/src/main/java/com/mycompany/variaveis/Variaveis.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+// comentario de 1 linha so
+/*
+comentario
+de varias
+linhas
+*/
+
+package com.mycompany.variaveis;
+
+/**
+ *
+ * @author ArthurAquino
+ */
+public class Variaveis {
+    // a classe main e quem executa os codigos
+    public static void main(String[] args) {
+     /* codigos de saida
+        -sao utilizados para imprimir/mostrar
+        dados e mensagens.
+        */
+     System.out.println("Ola");
+     System.out.println("eu sou um progamador");
+     
+     System.out.print("eu ja sei prog");
+     System.out.print(",eu nao amo java");
+     
+     //CRIAÇAO DE VARIAVEIS
+     int idade = 24; // valor inteiro
+     double altura = 1.77;// valor decimal pequeno
+     float peso = 60; //valor decimal pequeno
+     String nome = "Arthur borges"; //valor logico
+     boolean casada = false;// true - verdadeiro , false - falso
+     System.out.println("Ola "+nome);
+     System.out.println("voce tem "+idade+"anos de idade");
+    
+}//fim da main
+}
+
+//nenhum codigo pode ficar aqui fora
